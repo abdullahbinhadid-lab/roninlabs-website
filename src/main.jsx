0e140a1c90c700967dfef4b3e88688855e99c44f
abdullahbinhadid-lab <abdullah.binhadid@gmail.com>
2026-07-21 11:05:24 +0400

    Revert GitHub Pages config for Hostinger deployment, add .htaccess

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter basename="/roninlabs-website">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ErrorBoundary>
